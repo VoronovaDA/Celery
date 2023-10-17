98ff45419740bad5f960c9a7a2d094ca9db02be6
VoronovaDA <seregina.0130@gmail.com>
2023-10-17 16:06:23 +0300

    Added files

--- upscale.py
+++ upscale.py
@@ -1,7 +1,9 @@
 import cv2
+from celery import Celery
 from cv2 import dnn_superres
 
 
+celery_app = Celery(backend="redis://127.0.0.1:6379/1", broker="redis://127.0.0.1:6379/2")
 def upscale(input_path: str, output_path: str, model_path: str = 'EDSR_x2.pb') -> None:
     """
     :param input_path: путь к изображению для апскейла
